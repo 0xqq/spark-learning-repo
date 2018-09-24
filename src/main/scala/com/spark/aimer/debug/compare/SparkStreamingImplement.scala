8c8a893ea0b09ee1ba71c52091d60e3b889624f2
Kylin1027 <aimer1027@163.com>
2018-09-24 16:00:44 +0800

    push some codes on spark sql on row_number

---
+++ src/main/scala/com/spark/aimer/debug/compare/SparkStreamingImplement.scala
@@ -0,0 +1,116 @@
+package com.spark.aimer.debug.compare
+
+/**
+  * Created by Aimer1027 on 2018/9/18.
+  *
+  * In this class(object) we will re-appear this grouping by SQL semantics :
+  * select key-1, key-2, key-3, key-4 from (
+  *   select *, row_number over (partition by key-1, key-2 order by key-4 desc) as ranking from ${temp_view_name}
+  * ) where ranking = 1 ;
+  *
+  *
+  * Let's analyse deeply into this Spark-SQL semantics ：
+  *
+  * 1. Suppose we have a table(DataFrame) which with columns key-1, key-2, key-3, key-4
+  *    then, we create a temp view over it with the name of ${temp_view_name}
+  *
+  * 2. And we execute an inner SQL:
+  *    select *, row_number over (partition by key-1, key-2 order by key-4 desc) as ranking from ${temp_view_name}
+  *
+  *    In step2
+  *    2.1 We first partition the ${temp_view_name} into groups in which the value of ${key-1} ${key-2} should be the same
+  *
+  *    2.2 And traverse each group sorting the items by the ${key-4} and add a field with the name of 'ranking'
+  *       in which will store the order of each item after soring by item.${key-4} in descending.
+  *       Note: the ranking value is local scope in each group in which the ${key-1} and ${key-2} should be the same,
+  *             not the values in global scope in ${temp_view_name}.
+  *
+  *    2.3 Then we select all columns and the ranking value from the grouping result by select * , row_number.
+  *        After doing this inner SQL command, we will get
+  *            -------------------------------------------
+  * group-1    | key-1 | key-2 | key-3 | key-4  | ranking |
+  *            ------------------------------------------
+  *            | a      | b    | xxx   | 201810 |   1     |
+  *            --------------------------------------------
+  *            | a      | b    | xxx   | 201809 |   2     |
+  *            --------------------------------------------
+  *
+  *            ------------------------------------------
+  * group-2    | key-1 | key-2 | key-3 | key-4 | ranking |
+  *            ------------------------------------------
+  *            | a     |  c   |  xxxx  | 201808 | 1      |
+  *            -------------------------------------------
+  *            | a     |  c    | xxx   | 201807 | 2      |
+  *            -------------------------------------------
+  *
+  *            ------------------------------------------
+  * group-3    | key-1 | key-2 | key-3 | key-4 | ranking |
+  *            -------------------------------------------
+  *            | e     |  c   |  xxxx  | 201812 | 1      |
+  *            -------------------------------------------
+  *            | e     |  c    | xxx   | 201806 | 2      |
+  *            -------------------------------------------
+  *
+  * 3. Then, we will execute the outer one:
+  *    select key-1, key-2, key-3 key-4 from (
+  *
+  *          ${ the inner SQL command}
+  *
+  *    ) where ranking = 1 ;
+  *
+  *    So we will get the result
+  *
+  *             -------------------------------------------
+  *  group-1    | key-1 | key-2 | key-3 | key-4  | ranking |
+  *             ------------------------------------------
+  *             | a      | b    | xxx   | 201810 |   1     |
+  *             --------------------------------------------
+  *
+  *             ------------------------------------------
+  *  group-2    | key-1 | key-2 | key-3 | key-4 | ranking |
+  *             ------------------------------------------
+  *             | a     |  c   |  xxxx  | 201808 | 1      |
+  *             -------------------------------------------
+  *
+  *
+  *             ------------------------------------------
+  *  group-3    | key-1 | key-2 | key-3 | key-4 | ranking |
+  *             -------------------------------------------
+  *             | e     |  c   |  xxxx  | 201812 | 1      |
+  *             -------------------------------------------
+  *
+  *
+  *
+  *
+  *  In this implementation I will use the csv as upstream input stream instead of kafka streaming which in which
+  *  you have to handle the kafka producer and extracting the data contents from the data body.
+  *
+  *
+  *  And here is the schema of the input csv data
+  *  case class MyStudentItem
+  *  (
+  *      gradeID:Long,
+  *      classID:Long,
+  *      studentID:Long,
+  *      score:Int,
+  *      timestamp:java.sql.Timestamp
+  *  )
+  *  Referencing : 《Spark SQL 内核剖析》 第七章 Spark SQL 之 Aggregation
+  *  Page: [108 - 111]
+  */
+
+
+
+object SparkStreamingImplement {
+
+
+  case class MyStudentItem(gradeID:Long, classID:Long, studentID:Long, timestamp:java.sql.Timestamp)
+
+
+  def main( args:Array[String] ) = {
+
+    
+
+
+  }
+}
---
+++ src/main/scala/com/spark/aimer/debug/compare/SparkStructuredImplement.scala
@@ -0,0 +1,16 @@
+package com.spark.aimer.debug.compare
+
+import java.text.SimpleDateFormat
+import java.util.{Date, Properties}
+
+import com.alibaba.fastjson.JSONObject
+import kafka.serializer.StringEncoder
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+
+/**
+  * Created by Aimer1027 on 2018/9/18.
+  */
+object SparkStructuredImplement {
+
+}
