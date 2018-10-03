954ce923ee118d6589a015330152343b8a332801
kylin1027 <xiehui02@baidu.com>
2018-10-03 18:01:15 +0800

    add back-pressure docs for spark streaming

---
+++ src/main/scala/com/spark/aimer/util/KafkaStorageHandler.scala
@@ -0,0 +1,19 @@
+package com.spark.aimer.util
+
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.slf4j.{Logger, LoggerFactory}
+
+/**
+  * Created by Aimer1027 on 2018/10/3.
+  */
+class KafkaStorageHandler {
+
+  private val LOG:Logger = LoggerFactory.getLogger(classOf[KafkaStorageHandler])
+
+
+
+
+
+
+
+}
