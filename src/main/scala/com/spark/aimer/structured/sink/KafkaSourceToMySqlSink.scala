89147e5c17236964af0388ae5df4882dcb240665
Kylin1027 <aimer1027@126.com>
2018-10-21 04:18:56 +0800

    add mysql sinker without testing

--- src/main/scala/com/spark/aimer/structured/sink/KafkaSourceToMySqlSink.scala
+++ src/main/scala/com/spark/aimer/structured/sink/KafkaSourceToMySqlSink.scala
@@ -1,8 +1,10 @@
 package com.spark.aimer.structured.sink
 
-import java.sql.{Connection, DriverManager}
+import java.sql.{Connection, DriverManager, Statement}
 
-import org.apache.spark.sql.ForeachWriter
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.{DataFrame, ForeachWriter, Row, SparkSession}
 
 /**
   * 本 App 主要测试将数据流从 kafka 中加载进行简单过滤
@@ -17,41 +19,121 @@ import org.apache.spark.sql.ForeachWriter
   *    在写入数据库的过程中通过事务来完成, 如果该主键已经存在, 则会出现写入失败事务回滚,
   *    通过这种方式来避免数据通过 foreach(ForeachWriter[T]) 造成的数据重复的问题
   *
-  * 首先需要通过普通数据提交的 sinker 来将数据重复的问题进行复现
+  *
   * */
-object KafkaSourceToMySqlSink {
-
-}
 
 case class ResultData(id:String, msg:String, timestamp:String)
 
-class MySQLTransactionSinker(url:String, username:String, password:String) extends ForeachWriter[ResultData]{
-  override def open(partitionId: Long, version: Long): Boolean = ???
+object KafkaSourceToMySqlSink {
 
-  override def process(value: ResultData): Unit = ???
+  def rddParser(rddStr:String) = {
+    // TODO: add kafka streaming data parsing logic here
 
-  override def close(errorOrNull: Throwable): Unit = ???
+    val id:String = ""
+    val msg:String = ""
+    val timestamp:String = ""
+
+    new ResultData(id, msg, timestamp)
+  }
+
+  def main(args:Array[String]) = {
+    val conf = new SparkConf().setAppName("KafkaSourceToMySqlSink")
+    val spark:SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+    val url:String = args(1)
+    val username:String = args(2)
+    val password:String = args(3)
+
+    import spark.implicits._
+    // streaming reading
+    val df = spark.
+      readStream.
+      format("kafka").
+      option("kafka.bootstrap.servers", "").
+      option("subscribe", "dasou-stream").
+      option("startingOffsets", "latest").
+      load.
+      select($"value").
+      as[(String)].
+      map(rddParser)
+
+    // execute query upon streaming
+    val query = df.
+      writeStream.
+      foreach( new MySQLOrdinarySinker(url, username, password)).
+      // here we can use either MySQLOrdinarySinker or MySQLTransactionSinker,
+      // or we can create two queries one use ordinary sinker and another one use transaction sinker
+      outputMode(OutputMode.Append).
+      trigger(Trigger.ProcessingTime(1000)).
+      start
+
+    query.awaitTermination
+  }
+
 }
 
-class MySQLOrdinarySinker(url:String, username:String, password:String) extends ForeachWriter[ResultData] {
 
-  var conn:Connection = _
+class MySQLTransactionSinker(url:String, username:String, password:String) extends ForeachWriter[ResultData]{
+  var connection:Connection =_
+  var statement:Statement = _
 
   override def open(partitionId: Long, version: Long): Boolean = {
-
+    // we build connection to mysql in open method
     Class.forName("com.mysql.jdbc.Driver")
-    conn = DriverManager.getConnection(url)
+    connection = DriverManager.getConnection(url, username, password)
     true
   }
 
   override def process(value: ResultData): Unit = {
-    // transfer ResultData to sql
-    val sql = ""
-    conn.setAutoCommit(false)
 
+    val uuid:String = s"${value.id}-${value.timestamp}"
 
+    val sqlCmd = s"INSERT INTO spark.data1 (id, msg, `timestamp`) VALUES('${uuid}', '${value.msg}', '${value.timestamp}')"
+    try {
+      connection.setAutoCommit(false)
+      statement = connection.createStatement
+      statement.execute(sqlCmd)
+      connection.commit()
+    } catch {
+      case ex:Exception => {
+        println(s"Exception is ${ex.getMessage}")
+        connection.rollback
+      }
+    } finally {
+      statement.close()
+    }
+  }
+  override def close(errorOrNull: Throwable): Unit = {
+    connection.close
+  }
+}
 
+class MySQLOrdinarySinker(url:String, username:String, password:String) extends ForeachWriter[ResultData] {
+  var connection:Connection =_
+  var statement:Statement = _
+
+  override def open(partitionId: Long, version: Long): Boolean = {
+    // we build connection to mysql in open method
+    Class.forName("com.mysql.jdbc.Driver")
+    connection = DriverManager.getConnection(url, username, password)
+    statement = connection.createStatement
+    true
   }
 
-  override def close(errorOrNull: Throwable): Unit = ???
+  override def process(value: ResultData): Unit = {
+    val uuid:String = s"${value.id}-${value.timestamp}"
+    val sqlCmd = s"INSERT INTO spark.data1 (id, msg, `timestamp`) VALUES('${uuid}', '${value.msg}', '${value.timestamp}')"
+    try {
+      statement.execute(sqlCmd)
+    } catch {
+      case ex:Exception => {
+        println(s"Exception is ${ex.getMessage}")
+      }
+    } finally {
+      statement.close()
+    }
+  }
+  override def close(errorOrNull: Throwable): Unit = {
+    connection.close
+  }
 }
