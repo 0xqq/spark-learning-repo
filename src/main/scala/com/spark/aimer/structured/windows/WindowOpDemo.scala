3aa1fa9f1e5d77474398dc40f13d67fb1df33b23
Kylin1027 <aimer1027@126.com>
2018-11-02 16:28:31 +0800

    add windows operation demo in structured streaming

---
+++ src/main/scala/com/spark/aimer/structured/windows/WindowOpDemo.scala
@@ -0,0 +1,13 @@
+package com.spark.aimer.structured.windows
+
+/**
+  * In this class we will use the RateStreamSource which underlying generating data by RateSourceProvider
+  * to replace KafkaSourceProvide to generated upstream data.
+  *
+  *
+  *
+  * */
+
+object WindowOpDemo {
+
+}
